33b62befa54b4ca85a01585f496a9ff4bfbbccb1
Rehana Sadaq <rehana_sadaq@yahoo.co.uk>
2021-01-21 11:27:58 +0000

    Added Test folder

---
+++ PartyPlanner2Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PartyPlanner2Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PartyPlanner2Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2469beac-e0a4-4a19-9ef4-0c0ac01df050")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PartyPlanner2Tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PartyPlanner2;
+
+namespace PartyPlanner2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDrinkCostIsCorrect()
+        {
+            const int NUM_OF_GUESTS = 2;
+            Party p = new Party();
+            p.drinksCost = p.drinkPrices["Alcoholic"];
+            Assert.IsNotNull(p.drinksCost);
+            Assert.AreEqual(p.CostOfParty(), 0);
+            p.noOfGuests = NUM_OF_GUESTS;
+            Assert.AreEqual(p.CostOfParty(), p.drinksCost* NUM_OF_GUESTS);
+
+        }
+
+        [TestMethod]
+        public void TestMenuCostIsCorrect()
+        {
+            const int NUM_OF_GUESTS = 5;
+            Party p = new Party();
+            p.foodCost = p.foodPrices["Meat"];
+            Assert.IsNotNull(p.foodCost);
+            Assert.AreEqual(p.CostOfParty(), 0);
+            p.noOfGuests = NUM_OF_GUESTS;
+            Assert.AreEqual(p.CostOfParty(), p.foodCost* NUM_OF_GUESTS);
+        }
+
+        [TestMethod]
+        public void TestEntertainmentCostIsCorrect()
+        {
+            Party p = new Party();
+            p.entertainmentCost = p.entertainmentPrices["DJ"];
+            Assert.IsNotNull(p.entertainmentCost);
+            Assert.AreEqual(p.CostOfParty(), p.entertainmentCost);
+
+        }
+
+        [TestMethod]
+        public void TotalCostCalculated()
+        {
+            const int NUM_OF_GUESTS = 5;
+            Party p = new Party();
+            p.drinksCost = p.drinkPrices["Alcoholic"];
+            p.foodCost = p.foodPrices["Meat"];
+            p.entertainmentCost = p.entertainmentPrices["DJ"];
+            p.decorations = true;
+            p.noOfGuests = NUM_OF_GUESTS;
+            Assert.AreEqual(p.CostOfParty(), (p.drinksCost + p.foodCost) * NUM_OF_GUESTS + p.entertainmentCost + Party.decorationsCost);
+        }
+    }
+}
